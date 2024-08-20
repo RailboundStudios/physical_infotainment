96230b0b65cfc53668677bdb5baa24997f9b7674
ImBenji <benjamin.watt@imbenji.net>
2024-08-20 23:30:56 +0100

    test

--- donet_server/setup.sh
+++ donet_server/setup.sh
@@ -4,8 +4,13 @@
 dotnetbin=/home/imbenji/.dotnet/dotnet
 
 # Compile the c# code for arm64
-#dotnet publish -c Release -r linux-arm64
-$dotnetbin publish -c Release -r linux-arm64
+
+# if "donet_server/includes/rpi-rgb-led-matrix/bindings/c#/bin" does not exist, create it
+if [ ! -d "donet_server/includes/rpi-rgb-led-matrix/bindings/c#/bin" ]; then
+  $dotnetbin build donet_server/includes/rpi-rgb-led-matrix/bindings/c#
+fi
+$dotnetbin publish -c Release -r linux-arm64 # Compile the server code
+
 
 # Run the c# code
 #dotnet bin/Release/net5.0/linux-arm64/publish/HelloWorld.dll

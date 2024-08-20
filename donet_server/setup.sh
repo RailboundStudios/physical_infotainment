bb963aec5f91af0ef2d08bb5332de8c96151dd2d
ImBenji <benjamin.watt@imbenji.net>
2024-08-20 23:06:05 +0100

    Update setup.sh

--- donet_server/setup.sh
+++ donet_server/setup.sh
@@ -1,10 +1,12 @@
 ﻿#!/bin/bash
 
 # Load the PATH environment variable
-dotnetbin=$(dirname $(which dotnet))
+dotnetbin=/home/imbenji/.dotnet/dotnet
 
 # Compile the c# code for arm64
-dotnet publish -c Release -r linux-arm64
+#dotnet publish -c Release -r linux-arm64
+$dotnetbin publish -c Release -r linux-arm64
 
 # Run the c# code
-dotnet bin/Release/net5.0/linux-arm64/publish/HelloWorld.dll
+#dotnet bin/Release/net5.0/linux-arm64/publish/HelloWorld.dll
+$dotnetbin bin/Release/net5.0/linux-arm64/publish/HelloWorld.dll

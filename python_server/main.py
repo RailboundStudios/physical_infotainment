4e5cf377df1d7ad36deaed0750aa19b5d0267c59
ImBenji <benjamin.watt@imbenji.net>
2024-08-21 21:37:07 +0100

    Update main.py

--- python_server/main.py
+++ python_server/main.py
@@ -2,9 +2,6 @@ import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-from sys import path
-path.append('C:\\Development\\Repositories\\physical_infotainment\\python_server\\includes\\rpi-rgb-led-matrix\\bindings\\python\\rgbmatrix')
-
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
 topText = "Hello"

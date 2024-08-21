611439134b2b92935afe662b5be34ee8a46e0eb9
ImBenji <benjamin.watt@imbenji.net>
2024-08-21 20:48:37 +0100

    py

---
+++ python_server/main.py
@@ -0,0 +1,6 @@
+from sys import path
+path.append('C:\\Development\\Repositories\\physical_infotainment\\python_server\\includes\\rpi-rgb-led-matrix\\bindings\\python\\rgbmatrix')
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+matrix = RGBMatrix()

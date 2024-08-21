bd05bda874e98557cf53dfa9c7dd6199ced88cbb
ImBenji <benjamin.watt@imbenji.net>
2024-08-21 14:31:11 +0100

    test

--- dotnet_server/dotnet_server/Program.cs
+++ dotnet_server/dotnet_server/Program.cs
@@ -16,38 +16,38 @@ var matrix = new RGBLedMatrix(
 var canvas = matrix.CreateOffscreenCanvas();
 var font = new RGBLedFont("assets/4x6.bdf");
 
-var color = new Color(255, 0, 0);
-
-String text = "Hello World!";
-
-int pos = canvas.Width;
-
-bool running = true;
-
-Console.WriteLine("Doign stufffffff");
-
-Console.CancelKeyPress += (s, e) =>
-{
-    running = false;
-    e.Cancel = true; // don't terminate, we need to dispose
-};
-
-
-while (running)
-{
-    canvas.Clear();
-
-    var length = canvas.DrawText(font, pos, 6, color, text);
-    matrix.SwapOnVsync(canvas);
-    pos -= 1;
-    
-    if (pos + length < 0)
-    {
-        pos = canvas.Width;
-    }
-    
-    Task.Delay(10).Wait();
-    
-    
-    Console.WriteLine("pos = " + pos);
-}
+// var color = new Color(255, 0, 0);
+//
+// String text = "Hello World!";
+//
+// int pos = canvas.Width;
+//
+// bool running = true;
+//
+// Console.WriteLine("Doign stufffffff");
+//
+// Console.CancelKeyPress += (s, e) =>
+// {
+//     running = false;
+//     e.Cancel = true; // don't terminate, we need to dispose
+// };
+//
+//
+// while (running)
+// {
+//     canvas.Clear();
+//
+//     var length = canvas.DrawText(font, pos, 6, color, text);
+//     matrix.SwapOnVsync(canvas);
+//     pos -= 1;
+//     
+//     if (pos + length < 0)
+//     {
+//         pos = canvas.Width;
+//     }
+//     
+//     Task.Delay(100).Wait();
+//     
+//     
+//     Console.WriteLine("pos = " + pos);
+// }

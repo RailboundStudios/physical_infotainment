33223bca737d6cbd510afa423ef2f1909370a11a
ImBenji <benjamin.watt@imbenji.net>
2024-08-21 19:30:11 +0100

    Update Program.cs

--- dotnet_server/dotnet_server/Program.cs
+++ dotnet_server/dotnet_server/Program.cs
@@ -29,6 +29,7 @@ Task.Delay(5000).Wait();
 var font = new RGBLedFont("/home/imbenji/physical_infotainment/dotnet_server/dotnet_server/assets/4x6.bdf");
 canvas.Clear();
 canvas.DrawText(font, 1, 6, new Color(0, 255, 0), "Testing font");
+canvas.DrawText(font, 10, 10, new Color(0, 255, 0), "Testing font");
 matrix.SwapOnVsync(canvas);
 
 // Hold for 5 seconds

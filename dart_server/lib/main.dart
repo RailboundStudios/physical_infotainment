a4af7c24f12ba1d55cb93aa9cd57d432c7648904
ImBenji <benjamin.watt@imbenji.net>
2024-08-22 23:20:46 +0100

    Update main.dart

--- dart_server/lib/main.dart
+++ dart_server/lib/main.dart
@@ -4,6 +4,10 @@ import 'package:dart_server/main.dart' as dart_server;
 
 Future<void> main(List<String> arguments) async {
 
+  // Get current working directory
+  String currentDirectory = Directory.current.path;
+  print("Current directory: $currentDirectory");
+
   // Start the matrix server. The server relies on commands to control the matrix.
   // The server is written in dotnet, located: ../../dotnet_server/bin/Debug/net6.0/dotnet_server.dll
   // The server is started with the command: dotnet dotnet_server.dll

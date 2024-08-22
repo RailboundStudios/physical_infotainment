7a0b6007d4cc8330a0840168b15e2fb102856c6f
ImBenji <benjamin.watt@imbenji.net>
2024-08-22 23:24:15 +0100

    Update main.dart

--- dart_server/lib/main.dart
+++ dart_server/lib/main.dart
@@ -8,7 +8,9 @@ Future<void> main(List<String> arguments) async {
   // The server is written in dotnet, located: ../../dotnet_server/bin/Debug/net6.0/dotnet_server.dll
   // The server is started with the command: dotnet dotnet_server.dll
 
-  Process matrixServer = await Process.start("dotnet", ["/dotnet_server/bin/dotnet_server.dll"]);
+  String dotnetPath = "/home/imbenji/.dotnet/dotnet";
+
+  Process matrixServer = await Process.start(dotnetPath, ["/dotnet_server/bin/dotnet_server.dll"]);
 
   matrixServer.stdin.writeln("top=Hello from Dart!");
 

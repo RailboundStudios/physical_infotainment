02f8df94c9cd9c4efcca5ab6edbb4132819602fb
ImBenji <benjamin.watt@imbenji.net>
2024-08-23 17:51:21 +0100

    test

--- node_server/index.js
+++ node_server/index.js
@@ -1,23 +1,7 @@
-const bleno = require("bleno");
+const noble = require('noble');
 
-function runCommand(command) {
-    // todo run command
-    console.log("Running command: " + command);
+noble.startScanning();
+
+while (true) {
+  console.log('Scanning...');
 }
-
-bleno.on("stateChange", (state) => {
-   if (state === "poweredOn") {
-      bleno.startAdvertising("Pi Infotainment", ["1803"]);
-   } else {
-        bleno.stopAdvertising();
-   }
-
-   // Receive command
-    bleno.on("accept", (clientAddress) => {
-         console.log("Accepted connection from: " + clientAddress);
-         bleno.on("data", (data) => {
-              console.log("Received data: " + data);
-              runCommand(data);
-         });
-    });
-});

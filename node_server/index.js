83c5695c41072e26ef713444f06be6ab6dd6ac0b
ImBenji <benjamin.watt@imbenji.net>
2024-08-23 17:01:46 +0100

    Update index.js

--- node_server/index.js
+++ node_server/index.js
@@ -1,9 +1,22 @@
 const bleno = require("bleno");
 
+function runCommand(String command) {
+    // todo run command
+}
+
 bleno.on("stateChange", (state) => {
    if (state === "poweredOn") {
       bleno.startAdvertising("Pi Infotainment", ["12ab"]);
    } else {
         bleno.stopAdvertising();
    }
+
+   // Receive command
+    bleno.on("accept", (clientAddress) => {
+         console.log("Accepted connection from: " + clientAddress);
+         bleno.on("data", (data) => {
+              console.log("Received data: " + data);
+              runCommand(data);
+         });
+    });
 });

8909204c22016a9df547a0715c9372b80583c641
ImBenji <benjamin.watt@imbenji.net>
2024-08-23 14:42:57 +0100

    Update Program.cs

--- dotnet_server/dotnet_server/Program.cs
+++ dotnet_server/dotnet_server/Program.cs
@@ -13,6 +13,22 @@ if (!argsList.Contains("--led-no-hardware-pulse"))
 // Convert the list back to an array
 args = argsList.ToArray();
 
+new Thread(async () =>
+{
+    Console.WriteLine("Starting Bluetooth");
+    
+    IAdapter1 adapter = (await BlueZManager.GetAdaptersAsync()).FirstOrDefault();
+    
+    await adapter.StartDiscoveryAsync();
+    
+    foreach (Device device in await adapter.GetDevicesAsync())
+    {
+        Console.WriteLine("Device: " + await device.GetNameAsync());
+    }
+    
+    
+}).Start();
+
 Console.WriteLine("Creating matrix");
 using var matrix = new RGBLedMatrix(new RGBLedMatrixOptions
 {
@@ -191,21 +207,7 @@ new Thread(() =>
     Console.WriteLine("Matrix stopped");
 }).Start();
 
-new Thread(async () =>
-{
-    Console.WriteLine("Starting Bluetooth");
-    
-    IAdapter1 adapter = (await BlueZManager.GetAdaptersAsync()).FirstOrDefault();
-    
-    await adapter.StartDiscoveryAsync();
-    
-    foreach (Device device in await adapter.GetDevicesAsync())
-    {
-        Console.WriteLine("Device: " + await device.GetNameAsync());
-    }
-    
-    
-}).Start();
+
 
 
 while (running)

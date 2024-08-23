be491f9fcf3cdae415b38dcc6b1e2e1e25d09a63
ImBenji <benjamin.watt@imbenji.net>
2024-08-23 05:26:15 +0100

    Update main.dart

--- dart_server/lib/main.dart
+++ dart_server/lib/main.dart
@@ -13,7 +13,7 @@ Future<void> main(List<String> arguments) async {
   String dotnetPath = "/home/imbenji/.dotnet/dotnet";
   String currentDirectory = Directory.current.path;
 
-  Process matrixServer = await Process.start("./dotnet_server/bin/dotnet_server", [""],
+  Process matrixServer = await Process.start("./dotnet_server", [""],
       workingDirectory: "/home/imbenji/physical_infotainment/dotnet_server/bin/"
   );
 

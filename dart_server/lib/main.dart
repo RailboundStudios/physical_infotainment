6f6bba0c93c5507fc9807f2d6b474919239a60cd
ImBenji <benjamin.watt@imbenji.net>
2024-09-03 19:35:18 +0100

    Update main.dart

--- dart_server/lib/main.dart
+++ dart_server/lib/main.dart
@@ -2,16 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:crypto/crypto.dart';
 import 'package:dart_server/backend/backend.dart';
-import 'package:dart_server/io/gps_tracker.dart';
-import 'package:dart_server/io/matrix_display.dart';
-import 'package:dart_server/route.dart';
-import 'package:dart_server/utils/OrdinanceSurveyUtils.dart';
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as shelf_io;
-import 'package:shelf_router/shelf_router.dart';
-import 'package:vector_math/vector_math.dart';
 
 Future<void> main(List<String> arguments) async {
 
